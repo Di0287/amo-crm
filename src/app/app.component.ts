76501d695a1fdbd5c921c27c407a72b2dda9566b
MoonLight <di0287@ya.ru>
2024-12-19 16:21:41 +0500

    111

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
-import {RouterOutlet} from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgForOf, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'amo-crm';
+
+  protected readonly routeList: { path: string; name: string }[] = [
+    {path: '/leads', name: 'СДЕЛКИ'},
+    {path: '/contacts', name: 'КОНТАКТЫ'},
+    {path: '/stages', name: 'ЭТАПЫ'},
+  ];
+  ngOnInit() {
+
+  }
 }
